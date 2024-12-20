8962f2f87dd21c1aca8a2404bd13c8added93405
Sravan Vanteru <sravan.vanteru@gmail.com>
2024-12-20 08:38:20 +0530

    Inventory management improvement changes

--- src/Collections/GenericInventoryManagement.java
+++ src/Collections/GenericInventoryManagement.java
@@ -3,7 +3,7 @@ package Collections;
 import java.util.*;
 
 public class GenericInventoryManagement <T extends Item>{
-    private HashMap<String, T> inventory;
+    private final HashMap<String, T> inventory;
     public GenericInventoryManagement() {
         inventory = new HashMap<>();
     }
@@ -18,19 +18,19 @@ public class GenericInventoryManagement <T extends Item>{
         if (inventory.containsKey(id)){
             inventory.remove(id);
         }else{
-            throw new NoSuchElementException("Item not found");
+            throw new NoSuchElementException("Item not found to delete from the inventory");
         }
     }
     public T get(String id){
         if(inventory.containsKey(id)){
             return inventory.get(id);
         }else{
-            throw new NoSuchElementException("Item not found");
+            throw new NoSuchElementException("Item not found, please add it to the inventory");
         }
     }
     public Collection<T> getAllItems(){
         if(inventory.isEmpty()){
-            throw new NoSuchElementException("Items not found");
+            throw new NoSuchElementException("Inventory is empty, Please check your inventory");
         }else{
             return inventory.values();
         }
